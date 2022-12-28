17da9f4b5bce02a30efc7fa6c071d4ec29c8e961
Hugo Martinez <contact@hmartinez.fr>
2022-12-28 12:00:01 +0100

    Adding v1 of scripts

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # lightsoff-contact-commercants
+
+## Variables .env à paramétrer 
+TWILIO_ACCOUND_SID => Votre Accound SID Twilio
+TWILIO_AUTH_TOKEN => Votre Auth Token Twilio
+TWILIO_PHONE => Un téléphone Twilio éligible SMS & Voice
---
+++ index.js
@@ -0,0 +1,25 @@
+const accountSid = process.env.TWILIO_ACCOUND_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const phoneNumberFrom = process.env.TWILIO_PHONE;
+const client = require("twilio")(accountSid, authToken);
+
+var phones = ["+33652439730", "+33781041362"]
+
+phones.forEach(phone => {
+    if(phone[3] == "6" || phone[3] == "7") {
+        client.messages
+            .create({ body: "Hello from Twilio", from: phoneNumberFrom, to: phone })
+            .then(message => console.log(message.sid));
+    } else {
+        client.calls
+            .create({
+                url: 'https://github.com/La-Reserve-Tech-For-Good/lightsoff-contact-commercants/blob/main/voice.xml',
+                to: phone,
+                from: phoneNumberFrom
+            })
+            .then(call => console.log(call.sid));
+    }
+});
+
+
+  
