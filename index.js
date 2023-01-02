fdb9322f971d152b042c0c5eefce187dfa788b33
Hugo Martinez <contact@hmartinez.fr>
2023-01-02 19:29:24 +0100

    Edit message

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var phones = ["+33667877564"]
 phones.forEach(phone => {
     if(phone[3] == "6" || phone[3] == "7") {
         client.messages
-            .create({ body: "Hello from Twilio", from: phoneNumberFrom, to: phone })
+            .create({ body: "Bonjour, plusieurs clients ont indiqué que la devanture de votre commerce restait allumée la nuit. Si c’est le cas, auriez-vous la gentillesse de l’éteindre en partant le soir ? Nous sommes en pleine crise énergétique et il est essentiel que nous fassions tous attention à faire des économies d’énergie pour éviter les coupures cet hiver et préserver notre planète. Chaque geste compte. En plus, depuis février 2022 la loi a été endurcie et vous risquez une forte amende en cas de contrôle. Bonne journée.", from: phoneNumberFrom, to: phone })
             .then(message => console.log(message.sid));
     } else {
         client.calls
