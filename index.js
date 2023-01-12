6370c5b194e018d2c5d30dd10c447faf7b9c0499
Hugo Martinez <contact@hmartinez.fr>
2023-01-12 17:08:38 +0100

    New voice message + editing phone numbers implementation

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUND_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const phoneNumberFrom = process.env.TWILIO_PHONE;
+const phoneNumberSMSFrom = process.env.TWILIO_PHONE_SMS;
+const phoneNumberVoiceFrom = process.env.TWILIO_PHONE_VOICE;
 const client = require("twilio")(accountSid, authToken);
 
 const findPlaceById = (id) => {
@@ -41,14 +42,14 @@ reviews.forEach(review => {
             // évaluation du numéro : si fixe => appel / si portable => SMS
             if(phone_number[3] == "6" || phone_number[3] == "7") {
                 client.messages
-                    .create({ body: "Bonjour, plusieurs clients ont indiqué que la devanture de votre commerce restait allumée la nuit. Si c’est le cas, auriez-vous la gentillesse de l’éteindre en partant le soir ? Nous sommes en pleine crise énergétique et il est essentiel que nous fassions tous attention à faire des économies d’énergie pour éviter les coupures cet hiver et préserver notre planète. Chaque geste compte. En plus, depuis février 2022 la loi a été endurcie et vous risquez une forte amende en cas de contrôle. Bonne journée.", from: phoneNumberFrom, to: phone_number })
+                    .create({ body: "Bonjour, plusieurs clients ont indiqué que la devanture de votre commerce restait allumée la nuit. Si c’est le cas, auriez-vous la gentillesse de l’éteindre en partant le soir ? Nous sommes en pleine crise énergétique et il est essentiel que nous fassions tous attention à faire des économies d’énergie pour éviter les coupures cet hiver et préserver notre planète. Chaque geste compte. En plus, depuis février 2022 la loi a été endurcie et vous risquez une forte amende en cas de contrôle. Bonne journée.", from: phoneNumberSMSFrom, to: phone_number })
                     .then(message => console.log(message.sid));
             } else {
                 client.calls
                     .create({
                         url: 'https://github.com/La-Reserve-Tech-For-Good/lightsoff-contact-commercants/blob/main/voice.xml',
                         to: phone_number,
-                        from: phoneNumberFrom
+                        from: phoneNumberVoiceFrom
                     })
                     .then(call => console.log(call.sid));
             }
