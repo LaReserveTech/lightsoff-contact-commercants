f6b7ba8c697ab21d31f5eff7f7bb1bd672602df4
Hugo Martinez <contact@hmartinez.fr>
2023-02-07 11:37:30 +0100

    Fix error of scope

--- pullIncomings.js
+++ pullIncomings.js
@@ -22,25 +22,20 @@ const tempData = async () => {
   })
 }
 
-tempData()
-  .then((messages) => {
-    messages.forEach((messageID) => {
-      ovh.request(
-        'GET',
-        '/sms/' + process.env.OVH_SERVICE_NAME + '/incoming/' + messageID,
-        function (err, msgText) {
-          if (err) {
-            console.log(err)
-          } else {
-            axios({
-              method: 'post',
-              url: process.env.SLACK_WEBHOOK,
-              headers: { 'Content-type': 'application/json' },
-              data: { text: msgText.message }
-            })
-          }
-        }
-      )
+tempData().then(messages => {
+  messages.forEach(messageID => {
+    ovh.request('GET', '/sms/' + process.env.OVH_SERVICE_NAME + '/incoming/' + messageID, function (err, msgText) {
+      if (err) {
+        console.log(err)
+      } else {
+        axios({
+          method: 'post',
+          url: process.env.SLACK_WEBHOOK,
+          headers: { 'Content-type': 'application/json' },
+          data: { text: msgText.message }
+        })
+      }
+    })
 
     ovh.request('DELETE', '/sms/' + process.env.OVH_SERVICE_NAME + '/incoming/' + messageID, function (err, res) {
       if (err) {
@@ -50,6 +45,6 @@ tempData()
       }
     })
   })
-  .catch((err) => {
-    console.error(err)
-  })
+}).catch(err => {
+  console.error(err)
+})
